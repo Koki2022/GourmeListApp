03bc4c5f965a5b0f9f8556ef55c4922031943260
高橋昴希 <takahashikoki@takahashisubarunozominoMacBook-Air.local>
2024-02-19 05:05:37 +0900

    #1 画面遷移全体の配列パスを管理したファイル

---
+++ GourmeListApp/GourmeListApp/GourmeListPathModel.swift
@@ -0,0 +1,13 @@
+//
+//  GourmeListPathModel.swift
+//  GourmeListApp
+//
+//  Created by 高橋昴希 on 2024/02/15.
+//
+
+import Foundation
+
+// 画面遷移全体の配列パスとして扱う列挙型
+enum gourmeListPath {
+    case storeInfoView, storeEditView, storeSearchView, storeRegistrationView
+}
---
+++ GourmeListApp/GourmeListApp/TagViewPath.swift
@@ -0,0 +1,8 @@
+//
+//  TagViewPath.swift
+//  GourmeListApp
+//
+//  Created by 高橋昴希 on 2024/02/18.
+//
+
+import Foundation
