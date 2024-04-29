d1518f1bff5abcc355f96ffd5af2bbb686265325
高橋昴希 <takahashikoki@takahashisubarunozominoMacBook-Air.local>
2024-04-29 14:37:09 +0900

    #10 オリジナル検索バーの実装

--- GourmeListApp/GourmeListApp/OriginalSearchBarView.swift
+++ GourmeListApp/GourmeListApp/OriginalSearchBarView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct OriginalSearchBarView: View {
+    @Binding var inputTextTagSelectHomeView: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // 背景の設定
+            RoundedRectangle(cornerRadius: 8) // 角丸長方形
+                .fill(Color.gray.opacity(0.2))
+                .frame(width: 350, height: 40)
+            HStack(spacing: 6) {
+                // アイコンと入力欄の位置調整
+                Spacer()
+                    .frame(width: 25)
+                //　虫眼鏡
+                Image(systemName: "magnifyingglass")
+                    .foregroundStyle(.gray)
+                // 入力欄
+                TextField("タグの名前を検索", text: $inputTextTagSelectHomeView)
+            }
+
+        }
     }
 }
 
 #Preview {
-    OriginalSearchBarView()
+    OriginalSearchBarView(inputTextTagSelectHomeView: .constant(""))
 }
