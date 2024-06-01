d3916f22cade9a755cc5253b5b2d79d0bfe9bd05
高橋昴希 <takahashikoki@takahashisubarunozominoMacBook-Air.local>
2024-06-02 05:51:56 +0900

    #14 変数名の変更

--- GourmeListApp/GourmeListApp/HomeNavigatePathModel.swift
+++ GourmeListApp/GourmeListApp/HomeNavigatePathModel.swift
@@ -1,5 +1,5 @@
 //
-//  GourmeListPathModel.swift
+//  HomeNavigatePath.swift
 //  GourmeListApp
 //
 //  Created by 高橋昴希 on 2024/02/15.
@@ -9,6 +9,6 @@ import Foundation
 
 // 画面遷移全体の配列パスとして扱う列挙型。UpperCamelCaseで記載し直しました。
 // storeRegistrationView,storeSearchViewはシート表示のため削除
-enum HomeViewPath {
+enum HomeNavigatePath {
     case storeInfoView, storeEditView
 }
