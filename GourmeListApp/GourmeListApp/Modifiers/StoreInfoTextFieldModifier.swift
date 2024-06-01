30a721af8588a338d8a2af25856253a3ed758ba3
高橋昴希 <takahashikoki@takahashisubarunozominoMacBook-Air.local>
2024-06-01 18:27:20 +0900

    #14 お店登録画面のレイアウト実装

---
+++ GourmeListApp/GourmeListApp/Modifiers/StoreInfoTextFieldModifier.swift
@@ -0,0 +1,18 @@
+//
+//  StoreInfoTextFieldModifier.swift
+//  GourmeListApp
+//
+//  Created by 高橋昴希 on 2024/05/31.
+//
+
+import SwiftUI
+
+struct StoreInfoTextFieldModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreInfoTextFieldModifier()
+}
