cde4058803cba79c47fdede02b00bbfe0bd3b754
高橋昴希 <takahashikoki@takahashisubarunozominoMacBook-Air.local>
2024-06-10 16:57:48 +0900

    ファイル名称変更

--- GourmeListApp/GourmeListApp/TagAddView.swift
+++ GourmeListApp/GourmeListApp/TagAddView.swift
@@ -7,14 +7,10 @@
 
 import SwiftUI
 
-//　TagSelectView:タグ選択画面
-struct TagSelectView: View {
+//　TagAddView:タグ追加画面
+struct TagAddView: View {
     // タグ選択画面を閉じるための動作を呼び出す変数。
     @Environment(\.dismiss) private var dismiss
-    // タグ選択画面でのナビゲーションの状態を管理する配列パス
-    @State private var tagViewNavigatePath: [TagViewPath] = []
-    //　入力した文字を格納する変数
-    @State private var inputTagName: String = ""
     // タグボタンのサイズや行または列の要素数をArray文で定義
     private let columns: [GridItem] = Array(Array(repeating: .init(.fixed(120)), count: 3))
     // 各タグボタンを管理する配列。タグ名ごとに選択状態を管理するので構造体で管理
@@ -22,89 +18,116 @@ struct TagSelectView: View {
     // アラートを管理する変数
     @State private var isShowAlert: Bool = false
     var body: some View {
-        // NavigationStackと配列パスの紐付け
-        NavigationStack(path: $tagViewNavigatePath) {
-            //  スクロールビューの実装
-            ScrollView {
-                VStack {
-                    // 横線
-                    Divider()
-                    // 完了ボタン
-                    HStack {
+        //  スクロールビューの実装
+        ScrollView {
+            VStack {
+                // 横線
+                Divider()
+                // 完了ボタン
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        // 適用されたタグがあればホーム画面の選択中のタグに表示
+                        // viewを閉じて一覧画面へ遷移
+                        dismiss()
+                    }) {
                         Spacer()
-                        Button(action: {
-                            // 適用されたタグがあればホーム画面の選択中のタグに表示
-                            // viewを閉じて一覧画面へ遷移
-                            dismiss()
-                        }) {
-                            Spacer()
-                            Text("完了")
-                                .font(.system(size: 20))
-                                .foregroundStyle(.red)
-                                .padding(8)
-                        }
-                    }
-                    // 横線
-                    Divider()
-                    // 自作検索バー
-                    OriginalSearchBarView(inpuTagName: $inputTagName)
-                        .padding([.top, .bottom], 3)
-                    // タグボタンを１行に3つずつ配置
-                    LazyVGrid(columns: columns, alignment: .center, spacing: 5) {
-                        // ForEach文で任意の数のタグボタンを実装
-                        ForEach(Array(tagButtonInfo.enumerated()), id: \.offset) { index, _ in
-                            Button(action: {
-                                // タップされたボタンのBool値を変更
-                                tagButtonInfo[index].isTagButtonInfoShown.toggle()
-                            }) {
-                                Text("# ダミー")
-                                    .frame(width: 110, height: 45)
-                                    .font(.system(size: 18))
-                                    .foregroundStyle(.black)
-                                    .overlay(alignment: .center) {
-                                        // 角丸長方形
-                                        RoundedRectangle(cornerRadius: 10)
-                                            // 黒縁にする
-                                            .stroke(Color.black)
-                                    }
-                                    // タップしたボタンだけ背景色を黄色にする
-                                    .background(RoundedRectangle(cornerRadius: 10).fill(tagButtonInfo[index].isTagButtonInfoShown ? Color.yellow: Color.white))
-                                    .padding(10)
-                            }
-                            // 長押しした際の挙動
-                            .contextMenu(menuItems: {
-                                Button("削除", role: .destructive) {
-                                    isShowAlert.toggle()
-                                }
-                            })
-                        }
+                        Text("完了")
+                            .font(.system(size: 20))
+                            .foregroundStyle(.red)
+                            .padding(8)
                     }
                 }
+                // 横線
+                Divider()
+                // タグ追加ボタンを簡易的に実装(機能実装の際に修正)
+                HStack {
+                    Spacer()
+                    Button(action: {
+                    }) {
+                        Text("タグを追加")
+                            .frame(width: 110, height: 45)
+                            .font(.system(size: 18))
+                            .foregroundStyle(.gray)
+                            .background(Color.gray.opacity(0.2))
+                            .overlay(alignment: .center) {
+                                // 角丸長方形
+                                RoundedRectangle(cornerRadius: 10)
+                                    // 黒縁にする
+                                    .stroke(Color.black)
+                            }
+                    }
+                    Spacer()
+                    Button(action: {
+                        // タップされたボタンのBool値を変更
+
+                    }) {
+                        Text("# ダミー")
+                            .frame(width: 110, height: 45)
+                            .font(.system(size: 18))
+                            .foregroundStyle(.black)
+                            .overlay(alignment: .center) {
+                                // 角丸長方形
+                                RoundedRectangle(cornerRadius: 10)
+                                    // 黒縁にする
+                                    .stroke(Color.black)
+                            }
+                    }
+                    Spacer()
+                    Button(action: {
+                        // タップされたボタンのBool値を変更
+
+                    }) {
+                        Text("# ダミー")
+                            .frame(width: 110, height: 45)
+                            .font(.system(size: 18))
+                            .foregroundStyle(.black)
+                            .overlay(alignment: .center) {
+                                // 角丸長方形
+                                RoundedRectangle(cornerRadius: 10)
+                                    // 黒縁にする
+                                    .stroke(Color.black)
+                            }
+                    }
+                    Spacer()
+                }
+                // タグボタンを１行に3つずつ配置
+                LazyVGrid(columns: columns, alignment: .center, spacing: 5) {
+                    // ForEach文で任意の数のタグボタンを実装
+                    ForEach(Array(tagButtonInfo.enumerated()), id: \.offset) { index, _ in
+                        Button(action: {
+                            // タップされたボタンのBool値を変更
+                            tagButtonInfo[index].isTagButtonInfoShown.toggle()
+                        }) {
+                            Text("# ダミー")
+                                .frame(width: 110, height: 45)
+                                .font(.system(size: 18))
+                                .foregroundStyle(.black)
+                                .overlay(alignment: .center) {
+                                    // 角丸長方形
+                                    RoundedRectangle(cornerRadius: 10)
+                                        // 黒縁にする
+                                        .stroke(Color.black)
+                                }
+                                // タップしたボタンだけ背景色を黄色にする
+                                .background(RoundedRectangle(cornerRadius: 10).fill(tagButtonInfo[index].isTagButtonInfoShown ? Color.yellow: Color.white))
+                                .padding(10)
+                        }
+                        // 長押しした際の挙動
+                        .contextMenu(menuItems: {
+                            Button("削除", role: .destructive) {
+                                isShowAlert.toggle()
+                            }
+                        })
+                    }
+                }
+            }
+            Spacer()
                 // インジケータを右端に表示
                 .frame(maxWidth: .infinity)
-            }
-            // 遷移先のビューを定義
-            .navigationDestination(for: TagViewPath.self) { value in
-                switch value {
-                // タグ名入力欄ビューを定義
-                case .tagNameView:
-                    TagNameView(tagViewNavigatePath: $tagViewNavigatePath)
-                }
-            }
-            //　タグ作成ボタンを下部に配置
-            .toolbar {
-                ToolbarItem(placement: .bottomBar) {
-                    Button(action: {
-                        // 名前入力画面へ遷移
-                        tagViewNavigatePath.append(.tagNameView)
-                    }) {
-                        Text("新規タグ作成")
-                            .navigationBottomBarStyle()
-                    }
-                }
-            }
         }
-        // アラート処理
+
+        // ボタン長押し時のアラート処理
         .alert("削除しますか？ ", isPresented: $isShowAlert) {
             // 削除ボタン実装
             Button("削除", role: .destructive) {
@@ -122,5 +145,5 @@ struct TagSelectView: View {
 }
 
 #Preview {
-    TagSelectView()
+    TagAddView()
 }
