a28fc4c2a1a0baa8644a0ee97dab2464c43b545d
Koki2022 <kokikoki3219@icloud.com>
2024-09-29 08:54:32 +0900

    #35 お店登録画面で登録した画像をCoreDataで管理

---
+++ GourmeListApp/GourmeListApp/Model/StoreDetailData.swift
@@ -0,0 +1,18 @@
+//
+//  StoreDetailData.swift
+//  GourmeListApp
+//
+//  Created by 高橋昴希 on 2024/09/29.
+//
+
+import SwiftUI
+
+struct StoreDetailData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreDetailData()
+}
