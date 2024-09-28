f6c5f756989537f7f153b781c544cfba7b310c83
Koki2022 <kokikoki3219@icloud.com>
2024-09-29 08:55:42 +0900

    #35 お店情報のデータを管理する構造体を管理

--- GourmeListApp/GourmeListApp/Model/StoreDetailData.swift
+++ GourmeListApp/GourmeListApp/Model/StoreDetailData.swift
@@ -6,13 +6,30 @@
 //
 
 import SwiftUI
+import PhotosUI
 
-struct StoreDetailData: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    StoreDetailData()
+// StoreDetailData: お店情報を設定するためのデータをまとめた構造体
+struct StoreDetailData {
+    // フォトピッカー内で選択した複数アイテムを保持するプロパティ
+    var selectedItems: [PhotosPickerItem]
+    // PhotosPickerItem -> UIImageに変換した複数のアイテムを格納するプロパティ
+    var selectedImages: [UIImage]
+    // 削除の際に複数選択するためのインデックスをセット
+    var selectedIndexes: Set<Int> = []
+    //　店名の内容を反映する変数。
+    var storeName: String
+    //　訪問状況Pickerの識別値を管理する変数
+    var visitStatusTag: Int
+    //　訪問日を設定するカレンダー。現在の日時を取得
+    var visitDate: Date
+    // メモ記入欄の内容を反映する変数。
+    var memo: String
+    // 営業時間の内容を反映する変数。
+    var businessHours: String
+    //　電話番号を反映する変数。
+    var phoneNumber: String
+    //　郵便番号を反映する変数。
+    var postalCode: String
+    //　住所を反映する変数。
+    var address: String
 }
