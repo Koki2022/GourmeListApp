9592cfcaba3e7c042b324d1a61fe0ca6dacaa25b
Koki2022 <kokikoki3219@icloud.com>
2024-10-28 13:15:03 +0900

    #54 お店登録画面で登録した訪問状況をCoreDataで管理

--- GourmeListApp/GourmeListApp/Model/VisitationStatus.swift
+++ GourmeListApp/GourmeListApp/Model/VisitationStatus.swift
@@ -7,12 +7,9 @@
 
 import SwiftUI
 
-struct VisitationStatus: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    VisitationStatus()
+// 訪問状況は選択項目が決まっているのでenumで管理し、可読性と安全性を向上させる
+enum VisitationStatus: Int16 {
+    case visited = 0
+    case interested = 1
+    case none = 2
 }
