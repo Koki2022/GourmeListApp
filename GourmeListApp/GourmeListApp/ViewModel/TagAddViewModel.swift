9788860c55f5da1b1eff3f53622a94b7f40b0ee5
Koki2022 <kokikoki3219@icloud.com>
2024-11-12 05:34:16 +0900

    #54 TagAddViewで作成したタグのCoreData登録と、StoreRegistrationViewで登録したタグのCoreData登録

---
+++ GourmeListApp/GourmeListApp/ViewModel/TagAddViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  TagAddViewModel.swift
+//  GourmeListApp
+//
+//  Created by 高橋昴希 on 2024/11/08.
+//
+
+import SwiftUI
+
+struct TagAddViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TagAddViewModel()
+}
